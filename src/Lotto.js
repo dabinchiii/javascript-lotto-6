4aa8a99622f42513281e6038ee873688f278f3c2
dabinchiii <dabinchi388@naver.com>
2023-11-05 16:47:26 +0900

    feat(Lotto): 로또 번호의 유효성을 검사하는 로직 추가
    - 중복된 숫자를 포함할 경우 예외를 발생시키도록 함
    - 정수가 아닌 값을 포함할 경우 예외를 발생시키도록 함
    - 범위에 벗어난 값을 포함할 경우 예외를 발생시키도록 함

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,6 +10,20 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== numbers.length) {
+      throw new Error("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+
+    numbers.forEach((num) => {
+      if (!Number.isInteger(num)) {
+        throw new Error("[ERROR] 로또 번호의 각 숫자는 정수여야 합니다.");
+      }
+      if (num < 1 || num > 45) {
+        throw new Error(
+          "[ERROR] 로또 번호의 각 숫자는 1~45 사이의 숫자여야 합니다."
+        );
+      }
+    });
   }
 
   // TODO: 추가 기능 구현
