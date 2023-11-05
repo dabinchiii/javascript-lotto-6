7042348766fd1d068b8b5784b7d5cee7877d3703
dabinchiii <dabinchi388@naver.com>
2023-11-05 17:44:50 +0900

    feat(IssueLottos): 구매 금액에 해당하는 만큼의 로또를 발행하는 기능 구현
    - 구매 금액으로 발행할 로또의 수를 계산함
    - 발행할 수만큼의 로또를 발행하는 메소드 추가
    - 로또는 1~45 범위 내의 6개의 랜덤 숫자로 구성됨
    - 발행된 로또들은 issuedLottos 필드에 저장됨

--- src/LottoSystem.js
+++ src/LottoSystem.js
@@ -1,14 +1,20 @@
-import { Console } from "@woowacourse/mission-utils";
-
+import { Console, Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
 class LottoSystem {
   #purchaseAmount;
+  #countLotto;
+  #issuedLottos;
 
   constructor() {
     this.#purchaseAmount = null;
+    this.#countLotto = 0;
+    this.#issuedLottos = null;
   }
 
-  start = () => {
-    this.#getPurchaseAmount();
+  start = async () => {
+    await this.#getPurchaseAmount();
+    this.#getCountLotto(this.#purchaseAmount);
+    this.#issueLottos(this.#countLotto);
   };
 
   #getPurchaseAmount = async () => {
@@ -19,13 +25,32 @@ class LottoSystem {
         );
         const numberInput = Number(input);
         if (isNaN(numberInput) || numberInput <= 0 || numberInput % 1000) {
-          throw new Error("[ERROR] 잘못된 입력입니다.");
+          throw new Error(
+            "[ERROR] 구입 금액은 1,000 단위의 양의 정수여야 합니다."
+          );
         }
         this.#purchaseAmount = numberInput;
       } catch (err) {
         Console.print(err.message);
       }
     }
+  };
+
+  #getCountLotto = (moneyAmount) => {
+    this.#countLotto = moneyAmount / 1000;
+  };
+
+  #issueLottos = (count) => {
+    const result = [];
+    for (let i = 0; i < count; i++) {
+      result.push(this.#issueSingleLotto());
+    }
+    this.#issuedLottos = result;
+  };
+
+  #issueSingleLotto = () => {
+    const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+    return new Lotto(randomNumbers);
   };
 }
 
